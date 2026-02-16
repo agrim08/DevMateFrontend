d3ccb2e030a4392a833c6557f45448ec71733ce3
agrim08 <agrimgupta0805@gmail.com>
2026-02-16 22:29:26 +0530

    bug fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
 export const BASE_URL =
-  import.meta.env.VITE_API_URL || (location.hostname === "localhost" ? "http://localhost:4000" : "/api");
+  import.meta.env.VITE_API_URL || (location.hostname === "localhost" ? "http://localhost:4000/api" : "/api");
+
